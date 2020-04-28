63a9a5daa7ccb8047f33b6536d64e3c0ca23dd30
benike88 <benike88@users.noreply.github.com>
2020-04-28 18:54:03 +0200

    Big changes

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 let count = 0;
 
 $('#add').click(function() {
-    $('#ulist').append('<li id="todo'+ count +'">' + $('#inputbox').val() + "</li>");
+    $('#ulist').append('<div class="item"><li id="todo'+ count +'">' + $('#inputbox').val() + '</li><button class="actions" id="done">Done</button><button class="actions" id="delete">Delete</button></div><br>');
     $('#inputbox').val('');
     count++;
 })
